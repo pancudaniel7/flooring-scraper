948bd6f77bcaa0830bb6579c187d6c6422e65425
Daniel <daniel.pancu@ness.com>
2019-03-27 16:27:33 +0200

    Added Mohawk

--- src/Main.py
+++ src/Main.py
@@ -7,10 +7,14 @@ from src.transformer.ProductToShoppifyCsvTransformer import product_to_shopify
 def main():
     logger.info('Start collecting data')
     initialise_configurations()
+
     # Johnson Hardwood
     products_details = get_products_details()
     shoppify_csv_array = [product_to_shopify(product) for product in products_details]
     append_csv_array_to_file(csv_template_dir() + JOHNSOON_CSV_FILE_NAME, shoppify_csv_array)
+
+    #
+
     logger.info('Finish')
 
 
---
+++ src/service/supplier/MohawkScrapingService.py
@@ -0,0 +1,6 @@
+BASE_URL="https://www.mohawkflooring.com/"
+PRODUCTS_URL = 'https://www.mohawkflooring.com/wood/search?page=2'
+MOHAWK_VENDOR_NAME = 'Mohawk Flooring'
+MOHAWK_CARPET_CSV_FILE_NAME = 'mohawk-flooring-carpet-template.csv'
+MOHAWK_WOOD_CSV_FILE_NAME = 'mohawk-flooring-wood-template.csv'
+
