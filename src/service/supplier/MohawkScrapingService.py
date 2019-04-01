9334bc100d812b850e47ae9607742a14a149ba14
Daniel <daniel.pancu@ness.com>
2019-04-01 21:12:00 +0300

    Finish collecting all product urls

--- src/service/supplier/MohawkScrapingService.py
+++ src/service/supplier/MohawkScrapingService.py
@@ -8,7 +8,10 @@ from src.service.common.SeleniumCollectorService import get_page_source_until_se
 
 BASE_URL = 'https://www.mohawkflooring.com'
 CARPET_URL = BASE_URL + '/carpet/search?page='
-WOOD_URL = BASE_URL + '/wood/search?page='
+
+WOOD_CATEGORY_BASE_URL = BASE_URL + '/wood/search?page='
+WOOD_PRODUCT_BASE_URL = '/engineered-wood/detail'
+
 VINYL_URL = BASE_URL + '/vinyl/search?page='
 TILE_URL = BASE_URL + '/tile/search?page='
 RUGS_URL = BASE_URL + '/rugs/search?page='
@@ -20,14 +23,15 @@ WOOD_CSV_FILE_NAME = 'mohawk-flooring-wood-template.csv'
 TIME_OUT_DYNAMIC_DELAY = 2
 
 
-def get_product_categories_urls_for_page(driver: WebDriver, url: str, page_number: int):
+def get_product_category_urls_per_page(driver: WebDriver, url: str, page_number: int):
     try:
         driver.get(url + str(page_number))
         page_content = get_page_source_until_selector(driver, '.product-image', TIME_OUT_DYNAMIC_DELAY)
         soup = get_soup_by_page_content(page_content)
         return [BASE_URL + url for url in all_href_urls('.style-tile', soup)]
     except Exception as e:
-        logger.debug('Did not find any page source on page: {} with exception: {}'.format(page_number, e))
+        logger.debug(
+            'Did not find any page source on page for categories: {} with exception: {}'.format(page_number, e))
         return None
 
 
@@ -35,34 +39,48 @@ def get_all_product_category_urls(driver: WebDriver, url: str):
     category_urls = []
     i = 1
     while True:
-        response = get_product_categories_urls_for_page(driver, url, i)
+        response = get_product_category_urls_per_page(driver, url, i)
+        logger.debug('Get all product category urls for page: {}'.format(url + str(i)))
         if response is None:
             return category_urls
         i += 1
         category_urls.extend(response)
 
 
-def get_category_product_url(driver: WebDriver, category_url: str):
-    driver.get(category_url)
-    page_content = get_page_source_until_selector(driver, '#related-color', TIME_OUT_DYNAMIC_DELAY)
-    soup = get_soup_by_page_content(page_content)
-    data_style_ids = all_attributes_for_all_elements('.slider-container div>a', 'data-style-id', soup)
-    data_color_ids = all_attributes_for_all_elements('.slider-container div>a', 'data-color-id', soup)
-    product_category_title = tag_text('.column.main-info h2', soup).replace(' ', '-')
-    products_tiles = [text.replace(' ', '-') for text in tags_text('.slider-container span[class^="ng-binding"]', soup)]
-    return None
+def get_product_urls_per_page(driver: WebDriver, category_url: str, category_base_url: str):
+    try:
+        driver.get(category_url)
+        page_content = get_page_source_until_selector(driver, '#related-color', TIME_OUT_DYNAMIC_DELAY)
+        soup = get_soup_by_page_content(page_content)
+        data_style_ids = all_attributes_for_all_elements('.slider-container div>a', 'data-style-id', soup)
+        data_color_ids = all_attributes_for_all_elements('.slider-container div>a', 'data-color-id', soup)
+        product_category_title = tag_text('.column.main-info h2', soup).replace(' ', '-')
+        products_titles = [text.replace(' ', '-') for text in
+                           tags_text('.slider-container span[class^="ng-binding"]', soup)]
+        return [
+            BASE_URL + category_base_url + '/' + style_id + '-' + color_id + '/' + product_category_title + '-' + product_title
+            for
+            style_id, color_id, product_title in
+            zip(data_style_ids, data_color_ids, products_titles)]
+    except Exception as e:
+        logger.debug('Fail to get product urls on category url: {} with exception: {}'.format(category_url, e))
+        return None
 
 
-def get_product_urls(driver: WebDriver, category_urls: []):
+def get_all_product_urls(driver: WebDriver, category_urls: [], category_base_url: str):
     products_urls = []
     for category_url in category_urls:
-        pass
+        product_category_urls = get_product_urls_per_page(driver,
+                                                          category_url,
+                                                          category_base_url)
+        logger.debug('Get all product urls for category url: {}'.format(category_url))
+        products_urls.extend(product_category_urls)
+    return products_urls
 
 
 def get_wood_products_details():
     driver = webdriver.WebDriver()
-    # category_urls = get_all_product_category_urls(driver, WOOD_URL)
-    get_category_product_url(driver,
-                             'https://www.mohawkflooring.com/engineered-wood/detail/370-2291/Brookedale-Soft-Scrape-Uniclic-Cognac-Maple')
+    category_urls = get_all_product_category_urls(driver, WOOD_CATEGORY_BASE_URL)
+    product_urls = get_all_product_urls(driver, category_urls, WOOD_PRODUCT_BASE_URL)
     driver.quit()
     return None
