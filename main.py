73dba8b2b1a36b3ad9e218af7f50fbee6b062ef5
Codrin <ecodrin@yahoo.com>
2019-05-07 17:03:17 +0300

    modified main to look similar as develop one

--- main.py
+++ main.py
@@ -6,84 +6,34 @@ import csvService
 import productToShopifyCsvTransformer
 import shawScrapingService
 <<<<<<< HEAD
+<<<<<<< HEAD
 from config import initialise_configurations, logger, csv_template_dir, TEMPLATE_FILE_NAME, url_file_dir
 =======
 import republicScrapingService
 from config import initialise_configurations, logger, csv_template_dir, TEMPLATE_FILE_NAME
+=======
+from config import initialise_configurations, logger, csv_template_dir, TEMPLATE_FILE_NAME, url_file_dir
+
+
+def shaw_carpet_collecting():
+    products_details = shawScrapingService.get_products_details(shawScrapingService.CARPET_URL, 2000,
+                                                                url_file_dir() + shawScrapingService.SHAW_CARPET_URL_FILE_NAME)
+    shopify_csv_array = [productToShopifyCsvTransformer.product_to_shopify(product) for product in products_details]
+    products_details.clear()
+    csvService.clean_csv_file(csv_template_dir() + TEMPLATE_FILE_NAME,
+                              csv_template_dir() + shawScrapingService.SHAW_CARPET_CSV_FILE_NAME)
+    csvService.append_csv_array_to_file(csv_template_dir() + shawScrapingService.SHAW_CARPET_CSV_FILE_NAME,
+                                        shopify_csv_array)
+>>>>>>> modified main to look similar as develop one
 
 
 def main():
     initialise_configurations()
     logger.info('Start collecting data')
 
-    # Johnson Hardwood
-    # products_details = johnsonScrapingService.get_products_details()
-    # shopify_csv_array = [productToShopifyCsvTransformer.product_to_shopify(product) for product in products_details]
-    # csvService.clean_csv_file(csv_template_dir() + TEMPLATE_FILE_NAME,
-    #                           csv_template_dir() + johnsonScrapingService.WOOD_CSV_FILE_NAME)
-    # csvService.append_csv_array_to_file(csv_template_dir() + johnsonScrapingService.WOOD_CSV_FILE_NAME,
-    #                                     shopify_csv_array)
+    shaw_carpet_collecting()
 
-    # Mohawk Hardwood
-    # Wood
-    # products_details = mohawkScrapingService.get_wood_products_details()
-    # shopify_csv_array = [productToShopifyCsvTransformer.product_to_shopify(product) for product in products_details]
-    # free_mem(products_details)
-    # csvService.clean_csv_file(csv_template_dir() + TEMPLATE_FILE_NAME,
-    #                           csv_template_dir() + mohawkScrapingService.WOOD_CSV_FILE_NAME)
-    # csvService.append_csv_array_to_file(csv_template_dir() + mohawkScrapingService.WOOD_CSV_FILE_NAME,
-    #                                     shopify_csv_array)
-    #
-    # # Vinyl
-    # products_details = mohawkScrapingService.get_vinyl_products_details()
-    # shopify_csv_array = [productToShopifyCsvTransformer.product_to_shopify(product) for product in products_details]
-    # free_mem(products_details)
-    # csvService.clean_csv_file(csv_template_dir() + TEMPLATE_FILE_NAME,
-    #                           csv_template_dir() + mohawkScrapingService.VINYL_CSV_FILE_NAME)
-    # csvService.append_csv_array_to_file(csv_template_dir() + mohawkScrapingService.VINYL_CSV_FILE_NAME,
-    #                                     shopify_csv_array)
-    #
-    # # Tile
-    # products_details = mohawkScrapingService.get_tile_products_details()
-    # shopify_csv_array = [productToShopifyCsvTransformer.product_to_shopify(product) for product in products_details]
-    # free_mem(products_details)
-    # csvService.clean_csv_file(csv_template_dir() + TEMPLATE_FILE_NAME,
-    #                           csv_template_dir() + mohawkScrapingService.TILE_CSV_FILE_NAME)
-    # csvService.append_csv_array_to_file(csv_template_dir() + mohawkScrapingService.TILE_CSV_FILE_NAME,
-    #                                     shopify_csv_array)
-
-    # Regal Hardwood
-    # products_details = regalScrapingService.get_products_details()
-    # shopify_csv_array = [productToShopifyCsvTransformer.product_to_shopify(product) for product in products_details]
-    # csvService.clean_csv_file(csv_template_dir() + TEMPLATE_FILE_NAME,
-    #                           csv_template_dir() + regalScrapingService.REGAL_CSV_FILE_NAME)
-    # csvService.append_csv_array_to_file(csv_template_dir() + regalScrapingService.REGAL_CSV_FILE_NAME,
-    #                                     shopify_csv_array)
-
-    # Knoas
-    # products_details = knoasScrapingService.get_products_details()
-    # shopify_csv_array = [productToShopifyCsvTransformer.product_to_shopify(product) for product in products_details]
-    # csvService.clean_csv_file(csv_template_dir() + TEMPLATE_FILE_NAME,
-    #                           csv_template_dir() + knoasScrapingService.KNOAS_CSV_FILE_NAME)
-    # csvService.append_csv_array_to_file(csv_template_dir() + knoasScrapingService.KNOAS_CSV_FILE_NAME,
-    #                                     shopify_csv_array)
-
-    # Lw
-    # products_details = lwScrapingService.get_products_details()
-    # shopify_csv_array = [productToShopifyCsvTransformer.product_to_shopify(product) for product in products_details]
-    # csvService.clean_csv_file(csv_template_dir() + TEMPLATE_FILE_NAME,9
-    #                           csv_template_dir() + lwScrapingService.LW_CSV_FILE_NAME)
-    # csvService.append_csv_array_to_file(csv_template_dir() + lwScrapingService.LW_CSV_FILE_NAME,
-    #                                     shopify_csv_array)
-
-    # Eagle creek
-    # products_details = eagleFlooringScrapingService.get_hardwood_products_details()
-    # shopify_csv_array = [productToShopifyCsvTransformer.product_to_shopify(product) for product in products_details]
-    # csvService.clean_csv_file(csv_template_dir() + TEMPLATE_FILE_NAME,
-    #                           csv_template_dir() + eagleFlooringScrapingService.EAGLE_HARDWOOD_CSV_FILE_NAME)
-    # csvService.append_csv_array_to_file(csv_template_dir() + eagleFlooringScrapingService.EAGLE_HARDWOOD_CSV_FILE_NAME,
-    #                                     shopify_csv_array)
-
+<<<<<<< HEAD
     # products_details = eagleFlooringScrapingService.get_laminate_products_details()
     # shopify_csv_array = [productToShopifyCsvTransformer.product_to_shopify(product) for product in products_details]
     # csvService.clean_csv_file(csv_template_dir() + TEMPLATE_FILE_NAME,
@@ -160,8 +110,10 @@ def main():
 
 =======
 >>>>>>> Created republic service for Laminated
+=======
+>>>>>>> modified main to look similar as develop one
     logger.info('Finish')
-    # os.system('poweroff')
+    os.system('poweroff')
 
 
 main()
