743b35deb192055598277f1f7b86ba3a3482a40e
Codrin <ecodrin@yahoo.com>
2019-05-07 18:08:38 +0300

    Added Republic service

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ import csvService
 import productToShopifyCsvTransformer
 import shawScrapingService
 import republicScrapingService
-
 from config import initialise_configurations, logger, csv_template_dir, TEMPLATE_FILE_NAME, url_file_dir
 
 
-def shaw_carpet_collecting():
+def shaw_carpet_collecting(counter: int):
     products_details = shawScrapingService.get_products_details(shawScrapingService.CARPET_URL, 1000,
                                                                 url_file_dir() + shawScrapingService.SHAW_CARPET_URL_FILE_NAME)
     shopify_csv_array = [productToShopifyCsvTransformer.product_to_shopify(product) for product in products_details]
@@ -16,6 +15,27 @@ def shaw_carpet_collecting():
     csvService.append_csv_array_to_file(
         csv_template_dir() + str(counter) + shawScrapingService.SHAW_CARPET_CSV_FILE_NAME,
         shopify_csv_array)
+
+
+def republic_laminate_collecting():
+    products_details = republicScrapingService.get_products_details(republicScrapingService.LAMINATED_URL, 'Laminated')
+    shopify_csv_array = [productToShopifyCsvTransformer.product_to_shopify(product) for product in products_details]
+    products_details.clear()
+    csvService.clean_csv_file(csv_template_dir() + TEMPLATE_FILE_NAME,
+                              csv_template_dir() + republicScrapingService.REPUBLIC_LAMINATED_CSV_FILE_NAME)
+    csvService.append_csv_array_to_file(csv_template_dir() + republicScrapingService.REPUBLIC_LAMINATED_CSV_FILE_NAME,
+                                        shopify_csv_array)
+
+
+def republic_vinyl_collecting():
+    products_details = republicScrapingService.get_products_details(republicScrapingService.VINYL_1_URL, 'Vinyl')
+    products_details.extend(republicScrapingService.get_products_details(republicScrapingService.VINYL_2_URL, 'Vinyl'))
+    shopify_csv_array = [productToShopifyCsvTransformer.product_to_shopify(product) for product in products_details]
+    products_details.clear()
+    csvService.clean_csv_file(csv_template_dir() + TEMPLATE_FILE_NAME,
+                              csv_template_dir() + republicScrapingService.REPUBLIC_VINYL_CSV_FILE_NAME)
+    csvService.append_csv_array_to_file(csv_template_dir() + republicScrapingService.REPUBLIC_VINYL_CSV_FILE_NAME,
+                                        shopify_csv_array)
 
 
 def main():
