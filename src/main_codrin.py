4d456c9f69eea01ddb497d827d285bb5494defe4
Daniel <daniel.pancu@ness.com>
2019-05-10 21:11:45 +0300

    Added comntroller package and file example for shaw

--- src/main_codrin.py
+++ src/main_codrin.py
@@ -1,22 +1,57 @@
 #!/usr/bin/python3
+import config
 
-from config import url_file_dir, csv_template_dir, initialise_configurations, logger
 from service.csv import csvService
 from service.supplier import tarkettScrapingService
 from transformer import productToShopifyCsvTransformer
 
 
+# Tarkett
+# Laminate
 def tarkett_laminate_collecting():
-    product_details = tarkettScrapingService.get_products_details(tarkettScrapingService.LAMINATED_URL, ["Laminated"])
+    product_details = tarkettScrapingService.get_products_details(tarkettScrapingService.LAMINATED_URL, "Laminated")
+    csvService.clean_csv_file(config.csv_template_dir() + config.TEMPLATE_FILE_NAME,
+                              config.csv_template_dir() + tarkettScrapingService.TARKETT_LAMINATED_CSV_FILE_NAME)
+    shopify_csv_array = [productToShopifyCsvTransformer.product_to_shopify(product) for product in product_details]
+    product_details.clear()
+    csvService.append_csv_array_to_file(
+        config.csv_template_dir() + tarkettScrapingService.TARKETT_LAMINATED_CSV_FILE_NAME,
+        shopify_csv_array)
+
+
+# Tarkett
+# Vinyl
+def tarkett_vinyl_collecting():
+    product_details = tarkettScrapingService.get_products_details(tarkettScrapingService.VINYL_1_URL, "Vinyl")
+    product_details.extend(tarkettScrapingService.get_products_details(tarkettScrapingService.VINYL_2_URL, "Vinyl"))
+    csvService.clean_csv_file(config.csv_template_dir() + config.TEMPLATE_FILE_NAME,
+                              config.csv_template_dir() + tarkettScrapingService.TARKETT_VINYL_CSV_FILE_NAME)
+    shopify_csv_array = [productToShopifyCsvTransformer.product_to_shopify(product) for product in product_details]
+    product_details.clear()
+    csvService.append_csv_array_to_file(config.csv_template_dir() + tarkettScrapingService.TARKETT_VINYL_CSV_FILE_NAME,
+                                        shopify_csv_array)
+
+
+# Tarkett
+# Carpet
+def tarkett_carpet_collecting():
+    product_details = tarkettScrapingService.get_products_details(tarkettScrapingService.CARPET_URL, "Carpet")
+    csvService.clean_csv_file(config.csv_template_dir() + config.TEMPLATE_FILE_NAME,
+                              config.csv_template_dir() + tarkettScrapingService.TARKETT_CARPET_CSV_FILE_NAME)
+    shopify_csv_array = [productToShopifyCsvTransformer.product_to_shopify(product) for product in product_details]
+    product_details.clear()
+    csvService.append_csv_array_to_file(config.csv_template_dir() + tarkettScrapingService.TARKETT_CARPET_CSV_FILE_NAME,
+                                        shopify_csv_array)
 
 
 def main():
-    initialise_configurations()
-    logger.info('Start collecting data')
+    config.initialise_configurations()
+    config.logger.info('Start collecting data')
 
     tarkett_laminate_collecting()
-
-    logger.info('Finish')
+    tarkett_vinyl_collecting()
+    tarkett_carpet_collecting()
+    config.logger.info('Finish')
 
 
 main()
