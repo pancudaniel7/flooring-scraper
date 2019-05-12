d7c6ef0385e6f63a48f5c81bf6dba656bc808f7d
pancudaniel7 <pancudaniel7@gmail.com>
2019-05-12 13:02:18 +0300

    Added Bruce collected data

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import os
 
 from config import initialise_configurations
 from controller import bruceController
@@ -7,7 +6,7 @@ from controller import bruceController
 
 def main():
     initialise_configurations()
-    bruceController.bruce_collecting()
+    # bruceController.bruce_collecting()
     # os.system('poweroff')
 
 
