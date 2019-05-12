bbdb1997813f4aa7c79272dd930fb65971eab24c
Codrin <ecodrin@yahoo.com>
2019-05-12 18:44:36 +0300

    Lexmark service created

---
+++ src/controller/stainmasterController.py
@@ -0,0 +1,18 @@
+from config import csv_template_dir, url_file_dir
+from service.csv import csvService
+from service.supplier import stainmasterScrappigService
+from transformer import productToShopifyCsvTransformer
+
+
+# Stainmaster
+# Carpet
+def stainmaster_carpet_collecting(counter: int):
+    products_details = stainmasterScrappigService.get_products_details(stainmasterScrappigService.CARPET_URL, "Carpet",
+                                                                       1000, counter,
+                                                                       url_file_dir() + stainmasterScrappigService.STAINMASTER_CARPET_CSV_FILE_NAME)
+    shopify_csv_array = [productToShopifyCsvTransformer.product_to_shopify(product) for product in products_details]
+    products_details.clear()
+    csvService.append_csv_array_to_file(
+        csv_template_dir() + stainmasterScrappigService.STAINMASTER_CARPET_CSV_FILE_NAME,
+        shopify_csv_array)
+
