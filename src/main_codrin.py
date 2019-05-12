e92970e50774505b5fad7d801433b69b54165345
Codrin <ecodrin@yahoo.com>
2019-05-12 18:44:36 +0300

    Lexmark service created

---
+++ src/main_codrin.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import config
+from controller import tarkettCotroller
+
+
+def main():
+    config.initialise_configurations()
+    tarkettCotroller.tarkett_laminate_collecting()
+    tarkettCotroller.tarkett_carpet_collecting()
+    tarkettCotroller.tarkett_vinyl_collecting()
+
+
+main()
