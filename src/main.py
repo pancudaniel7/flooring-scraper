e200f97cbe96d65fc41c526c4935cf62b557657e
Codrin <ecodrin@yahoo.com>
2019-05-19 23:15:47 +0300

    Lexmark data

--- src/main.py
+++ src/main.py
@@ -2,12 +2,10 @@
 import os
 
 from config import initialise_configurations
-from controller import tarkettController, fuzionController
 
 
 def main():
     initialise_configurations()
-    fuzionController.collecting()
 
 
 main()
